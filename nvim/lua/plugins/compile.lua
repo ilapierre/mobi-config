2997b8e4b33325c2070a9152b64315252dda437f
Ian Lapierre <ian.lapierre@live.ca>
2026-09-01 06:31:59 -0400

    Compile mode

---
+++ nvim/lua/plugins/compile.lua
@@ -0,0 +1,19 @@
+return {
+	"ej-shafran/compile-mode.nvim",
+	version = "^5.0.0",
+	branch = "latest",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		---@type CompileModeOpts
+		vim.g.compile_mode = {
+			input_word_completion = true,
+			default_command = "",
+      focus_compilation_buffer = true,
+		}
+	end,
+	keys = {
+		{ "<leader>c", "<cmd>below Compile<cr>", desc = "Open Oil (floating)" },
+	},
+}
